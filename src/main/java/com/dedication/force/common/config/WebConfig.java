2dc9c7825422bb15ef5337d31d97211a61323483
LimDae94 <piay801@naver.com>
2024-07-08 12:11:01 +0900

    ♻️REF : 불필요한 임포트 파일 제거하기(#13)

--- src/main/java/com/dedication/force/common/config/WebConfig.java
+++ src/main/java/com/dedication/force/common/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.dedication.force.common.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
