a5b3dee50b8081c9ac742830d1d87d4e2cfe5ce6
LimDae94 <piay801@naver.com>
2024-07-08 10:42:33 +0900

    ✨ENH : 회원 로그인 컨트롤러 구현하기(#17)
    /login 엔드포인트로 email, password 로 회원 로그인에 시도한다.
    
    로그인에 성공하면 회원 고유 식별 번호, 액세스, 리프레시 토큰을 응답한다.
    
    모든 회원 조회 메서드명을 findAllMember 로 변경하여 더욱 직관적인 네이밍을 작성했다.

--- src/main/java/com/dedication/force/controller/AuthController.java
+++ src/main/java/com/dedication/force/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.dedication.force.controller;
 
 import com.dedication.force.common.HttpResponse;
 import com.dedication.force.domain.dto.AddMemberRequest;
+import com.dedication.force.domain.dto.LoginMemberRequest;
+import com.dedication.force.domain.dto.LoginMemberResponse;
 import com.dedication.force.domain.entity.Member;
 import com.dedication.force.service.MemberService;
 import jakarta.validation.Valid;
@@ -22,16 +24,26 @@ public class AuthController {
     private final MemberService memberService;
 
     // 회원 등록
-    @PostMapping("")
+    @PostMapping("/signup")
     public ResponseEntity<HttpResponse<Void>> addMember(@RequestBody @Valid AddMemberRequest request, BindingResult result) {
         memberService.addMember(request);
         return new ResponseEntity<>(new HttpResponse<>(1, "회원가입에 성공했습니다.", null), HttpStatus.CREATED);
     }
 
+    // 회원 로그인
+    @PostMapping("/login")
+    public ResponseEntity<HttpResponse<LoginMemberResponse>> login(@RequestBody @Valid LoginMemberRequest request, BindingResult result) {
+        LoginMemberResponse loginMemberResponse = memberService.login(request);
+        return new ResponseEntity<>(new HttpResponse<>(1, " 로그인에 성공했습니다.", loginMemberResponse), HttpStatus.CREATED);
+    }
+
+    // 회원 로그아웃
+
+
     // 모든 회원 조회
     @GetMapping("")
-    public ResponseEntity<HttpResponse<List<Member>>> allMember() {
-        return new ResponseEntity<>(new HttpResponse<>(1, "모든 회원 조회입니다.", memberService.allMember()), HttpStatus.OK);
+    public ResponseEntity<HttpResponse<List<Member>>> findAllMember() {
+        return new ResponseEntity<>(new HttpResponse<>(1, "모든 회원 조회입니다.", memberService.findAllMember()), HttpStatus.OK);
     }
 
     // 단건 회원 조회(id)
@@ -43,4 +55,8 @@ public class AuthController {
     // 회원 수정
 
     // 회원 삭제
+
+    // JWT 엑세스 토큰 재발급
+
+    // JWT 리프레시 토큰 재발급
 }
